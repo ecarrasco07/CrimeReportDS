3f9fa692babb89d73c5f8d1745b6f97816e6bd60
Logan Lauton <Logan.lauton@marquette.edu>
2023-02-07 15:20:05 -0600

    Removal of Primary Keys in Table

--- INDS4997.sql
+++ INDS4997.sql
@@ -1,13 +1,12 @@
 CREATE TABLE Crime(
-Call_Number         VARCHAR2(255) NOT NULL,
+Call_Number         VARCHAR2(255),
 Date_Time           VARCHAR2(255),
 Address             VARCHAR2(255),   
 Police_District     VARCHAR2(255),
 Nature_of_Call      VARCHAR2(255),
 Status              VARCHAR2(255),
 Latitude            FLOAT,
-Longitude           FLOAT,
-PRIMARY KEY (Call_Number)
+Longitude           FLOAT
 );
 
 --Selecting all elements from the Calls table and copying them into Crime for backups(Logan)
